1b4eba914c0b30cf7c1fba5f520b74fe01ad1220
geekylthyosaur <geekylthyosaur@gmail.com>
2022-07-03 14:12:09 +0300

    Simple WASM example.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::{window, HtmlElement};
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+#[wasm_bindgen(start)]
+pub fn run() {
+    let document = window().unwrap().document().unwrap();
+    let root = document
+        .get_element_by_id("root")
+        .unwrap()
+        .dyn_into::<HtmlElement>()
+        .unwrap();
+    root
+        .style()
+        .set_property("background-color", "red")
+        .unwrap();
 }
 
